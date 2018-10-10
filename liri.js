6ba4f632bbc0b718a31ea0c464f5bf0e27f8c5a6
Manda104 <motoko104@yahoo.com>
2018-10-09 19:56:35 -0500

    initial set up of APIs and setting up packages

--- liri.js
+++ liri.js
@@ -1,6 +1,29 @@
 require("dotenv").config();
 let keys = require("./keys");
+let request = require("request");
 
 let spotify = new Spotify(keys.spotify);
+let omdb = new omdb(keys.omdb);
+let bands = new bands(keys.bands);
 
-console.log(spotify);
+let input = process.argv.split(3);
+console.log("input");
+
+request("https://api.spotify.com/v1/search?q" + input +"", function (error,response, body){
+    if(!error && response.statusCode === 200) {
+
+    }
+});
+
+//search through spotify
+const spotifySearch = () => {
+    spotify.search({type: 'track', query:''}, function (err, data) {
+        if (err){
+            return console.log('Error occured' + err);
+        }
+        console.log(data);
+    })
+}
+
+spotify.search({type: 'track', query:''})
+request("")
